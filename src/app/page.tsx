94d7ba2b37552f8cbbed0c02ff80e28a6efed7e0
mach-12 <mann.compi@gmail.com>
2024-08-10 15:56:29 +0530

    removed news section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Footer from "@/custom_components/CommonComponents/Footer";
 import AboutUsSection from "@/custom_components/HomePageComponents/AboutUsSection";
 import JoinUsSection from "@/custom_components/HomePageComponents/JoinUs";
 import LeadershipSection from "@/custom_components/HomePageComponents/Leadership";
-import NewsSection from "@/custom_components/HomePageComponents/News";
 import ProjectsSection from "@/custom_components/HomePageComponents/Projects";
 import PublicationsSection from "@/custom_components/HomePageComponents/Publications";
 
