10573ecf70f05c2f5d33ab265b8f1597fe113406
Samaksh Tyagi <140980901+samakshty@users.noreply.github.com>
2024-09-08 20:32:59 +0530

    Update JoinUsBadge.tsx

--- src/app/join/JoinUsBadge.tsx
+++ src/app/join/JoinUsBadge.tsx
@@ -8,10 +8,10 @@ export default function JoinUsBadge() {
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between space-y-4 md:space-y-0 md:space-x-4">
         <div className="flex flex-col items-start space-y-2">
           <h2 className="text-lg font-bold text-primary-foreground">
-            Applications Open Now! (Junior Core - 1st Year students)
+            Applications Closed Now! (Junior Core - 1st Year students)
           </h2>
           <p className="text-sm text-primary-foreground">
-            Deadline: 11:59 PM, 5th September, 2024 
+             Look out for Volunteer Team forms before events!
           </p>
         </div>
         <Link
